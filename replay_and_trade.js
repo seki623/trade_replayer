4cce56ce35400305714cd53be704d2caa4b0b2d6
seki623 <bitter6choco2guri3@outlook.com>
2026-09-10 03:37:49 +0900

    Add files via upload

--- replay_and_trade.js
+++ replay_and_trade.js
@@ -57,19 +57,6 @@ async function loadSelectedRange() {
         const historyData = allRawData.filter(d => d.time < startTs);
         replayQueue = allRawData.filter(d => d.time >= startTs && d.time <= endTs);
 
-	
-	// replay_and_trade_3.js 内
-	if (typeof candleSeries !== 'undefined' && candleSeries) {
-    		candleSeries.setData(historyData.map(d => ({
-        	time: d.time, open: d.open, high: d.high, low: d.low, close: d.close
-    	})));
-    
-    	// PDO/PDH/PDL/PDC ラインを描画
-    	if (typeof renderPeriodLines === 'function') {
-        	renderPeriodLines(historyData);
-   	 }
-	
-
         if (replayQueue.length === 0) {
             alert(`【${symbol}】指定の期間データがCSV内に見つかりませんでした。`);
             return;
@@ -456,6 +443,3 @@ function updateAccountUI(currentPrice) {
         $pnl.className = 'pnl-val ' + (unrealizedPnl >= 0 ? 'pnl-plus' : 'pnl-minus');
     }
 }
-
-
-
